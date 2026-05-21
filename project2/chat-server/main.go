5403c96d9783594bffcce2db47d3f02dd5ebaa53
sasha Guetta <sashaguetta@icloud.com>
2026-05-21 10:15:21 -0400

    saving my work

---
+++ project2/chat-server/main.go
@@ -0,0 +1,52 @@
+package main
+
+import ( 
+	"encoding/json"
+	//"time"
+	"log"
+	"net/http"
+)
+
+type Message struct {
+	//username string 
+	//timestamp time.Time
+	Text string `json:"text"`
+	}
+
+var messages []Message
+
+func main(){
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+
+	http.HandleFunc("/messages", MessagesHandler)
+	http.HandleFunc("/message", MessageHandler)
+
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func MessagesHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return 
+   }
+   
+   json.NewEncoder(w).Encode(messages)
+
+}
+
+func MessageHandler(w http.ResponseWriter, r *http.Request) {
+   if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return 
+   }
+   var req Message
+   
+   err := json.NewDecoder(r.Body).Decode(&req)
+   if err != nil {
+		http.Error(w,"bad request", http.StatusBadRequest)
+		return
+   }
+   messages = append(messages, req)
+}
