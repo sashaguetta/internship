42fa8a4f924ed1a7511669605e29630a67a048c9
sasha Guetta <sashaguetta@icloud.com>
2026-05-26 10:30:47 -0400

    done

--- project2/chat-server/main.go
+++ project2/chat-server/main.go
@@ -1,6 +1,6 @@
 package main
 
-import ( 
+import (
 	"encoding/json"
 	//"time"
 	"log"
@@ -9,62 +9,74 @@ import (
 )
 
 type Message struct {
-	//username string 
+	//username string
 	//timestamp time.Time
 	Text string `json:"text"`
-	}
+	ImagePath string 
+}
 
-var messages []Message
-
-func main(){
-	b, err := os.ReadFile("messages.json")
-	if err != nil {
-		if !os.IsNotExist(err) {
-			log.Fatal(err)} 
-	} else {
-			err = json.Unmarshal(b, &messages)
-		}
-			
+func main() {
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 
 	http.HandleFunc("/messages", MessagesHandler)
 	http.HandleFunc("/message", MessageHandler)
-	
-	log.Fatal(http.ListenAndServe(":8080", nil))	
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func loadMessages() []Message {
+	var messages []Message
+	b, err := os.ReadFile("messages.json")
+	if err != nil {
+		if !os.IsNotExist(err) {
+			log.Fatal(err)
+		}
+	} else {
+		err = json.Unmarshal(b, &messages)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return messages
+}
+
+func saveMessages(messages []Message) {
+	b, err := json.MarshalIndent(messages, " ", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.WriteFile("messages.json", b, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func MessagesHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-		return 
-   }
-   
-   json.NewEncoder(w).Encode(messages)
+		return
+	}
+	messages := loadMessages()
+	json.NewEncoder(w).Encode(messages)
 
 }
 
 func MessageHandler(w http.ResponseWriter, r *http.Request) {
-   if r.Method != http.MethodPost {
+	if r.Method != http.MethodPost {
 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-		return 
-   }
-   var req Message
-   
-   err := json.NewDecoder(r.Body).Decode(&req)
-   if err != nil {
-		http.Error(w,"bad request", http.StatusBadRequest)
 		return
-   }
+	}
+	var req Message
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+	messages := loadMessages()
 	messages = append(messages, req)
-   
-	b, err := json.MarshalIndent(messages, " ", " ")
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = os.WriteFile("messages.json", b , 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
+	saveMessages(messages)
+
 }
