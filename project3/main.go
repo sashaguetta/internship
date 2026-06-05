455c511e3e5293f2e213ffb72e8cf55d845c5b2f
sasha Guetta <sashaguetta@icloud.com>
2026-06-05 14:21:47 -0400

    bot added

--- project3/main.go
+++ project3/main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
-	"github.com/openai/openai-go"
-
+	"context"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 
+	"github.com/openai/openai-go"
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
 )
 
 var slackClient = slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+var APIkey = os.Getenv("OPENAI_API_KEY")
 var openaiClient = openai.NewClient()
 
+func askOpenAI(userMessage string) (string, error) {
+	chatCompletion, err := openaiClient.Chat.Completions.New(context.Background(), openai.ChatCompletionNewParams{
+		Model: openai.ChatModelGPT4oMini,
+		Messages: []openai.ChatCompletionMessageParamUnion{
+			openai.SystemMessage("You are an assistant."),
+			openai.UserMessage(userMessage),
+		},
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return chatCompletion.Choices[0].Message.Content, nil
+}
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Slack sent a request!")
 
@@ -58,6 +74,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	response, err := askOpenAI("Quelle est la capital de la france ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(response)
 	http.HandleFunc("/slack/events", handler)
 
 	fmt.Println("Server running on :8080")
